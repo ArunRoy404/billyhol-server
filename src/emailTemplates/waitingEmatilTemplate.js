889bcd7231e29e331909e45347abc1b2365832e6
Arun Roy <arunroy0065@gmail.com>
2026-03-01 12:23:29 +0600

    fix: some issue fixed

--- src/emailTemplates/waitingEmatilTemplate.js
+++ src/emailTemplates/waitingEmatilTemplate.js
@@ -6,7 +6,6 @@ export const waitingEmailTemplate = ({ email, companyName, role, country, linked
                 <head>
                     <meta charset="UTF-8" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>Wenthura – Waiting List</title>
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
                 </head>
 
